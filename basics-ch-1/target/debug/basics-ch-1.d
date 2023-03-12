87ae02e7f7ecb79e693a6fe05b1284c4935293a8
Rahul <rahkulkarni@expediagroup.com>
2023-03-11 19:45:17 -0800

    Initial setup

---
+++ basics-ch-1/target/debug/basics-ch-1.d
@@ -0,0 +1,1 @@
+/Users/rahkulkarni/rust-atomics/basics-ch-1/target/debug/basics-ch-1: /Users/rahkulkarni/rust-atomics/basics-ch-1/src/main.rs
---
+++ basics-ch-1/target/debug/deps/basics_ch_1-90a5b4c741a8efc9.d
@@ -0,0 +1,5 @@
+/Users/rahkulkarni/rust-atomics/basics-ch-1/target/debug/deps/basics_ch_1-90a5b4c741a8efc9: src/main.rs
+
+/Users/rahkulkarni/rust-atomics/basics-ch-1/target/debug/deps/basics_ch_1-90a5b4c741a8efc9.d: src/main.rs
+
+src/main.rs:
